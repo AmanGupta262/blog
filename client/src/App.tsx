fff7127eed3b445f9c3342dbc0b5b59ce44efd14
Aman Gupta <amang5840@gmail.com>
2021-07-07 22:56:40 +0530

    setup tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import PageRender from "./PageRender";
+import Navbar from "./components/global/Navbar";
 
 const App = () => {
   return (
-    <div className="container">
+    <div className="container mx-auto px-4">
       <Router>
+        <Navbar />
         <Switch>
           <Route exact path="/" component={PageRender} />
           <Route exact path="/:page" component={PageRender} />
---
+++ client/src/components/global/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    const loginLinks = [
+      { label: "Home", path: "/" },
+      { label: "Login", path: "/login" },
+      { label: "Register", path: "/register" },
+    ];
+  return (
+      <div></div>
+    );
+};
+
+export default Navbar;
