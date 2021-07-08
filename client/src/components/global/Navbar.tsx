c753a7cd09511a0f7653b9d5837a853576c617e9
Aman Gupta <amang5840@gmail.com>
2021-07-08 15:08:23 +0530

    create search component

--- client/src/components/global/Navbar.tsx
+++ client/src/components/global/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import Search from "./Search";
 
 const Navbar = () => {
   const [menu, setMenu] = useState(false);
@@ -9,6 +10,11 @@ const Navbar = () => {
     { label: "Home", path: "/" },
     { label: "Login", path: "/login" },
     { label: "Register", path: "/register" },
+  ];
+  const userNavigation = [
+    { label: "Profile", path: "/profile" },
+    { label: "Settings", path: "/settings" },
+    { label: "Sign Out", path: "/logout" },
   ];
   return (
     <>
@@ -69,12 +75,14 @@ const Navbar = () => {
                 <div className="flex space-x-4">
                   {navigation.map((link, index) => (
                     <Link
+                      key={index}
                       to={link.path}
                       className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                     >
                       {link.label}
                     </Link>
                   ))}
+                  <Search />
                 </div>
               </div>
             </div>
@@ -125,33 +133,18 @@ const Navbar = () => {
                     aria-labelledby="user-menu-button"
                     tabIndex={-1}
                   >
-                    <a
-                      href="/"
-                      className="block px-4 py-2 text-sm text-gray-700"
-                      role="menuitem"
-                      tabIndex={-1}
-                      id="user-menu-item-0"
-                    >
-                      Your Profile
-                    </a>
-                    <a
-                      href="/"
-                      className="block px-4 py-2 text-sm text-gray-700"
-                      role="menuitem"
-                      tabIndex={-1}
-                      id="user-menu-item-1"
-                    >
-                      Settings
-                    </a>
-                    <a
-                      href="/"
-                      className="block px-4 py-2 text-sm text-gray-700"
-                      role="menuitem"
-                      tabIndex={-1}
-                      id="user-menu-item-2"
-                    >
-                      Sign out
-                    </a>
+                    {userNavigation.map((link, index) => (
+                      <Link
+                        key={index}
+                        to={link.path}
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-50 focus:bg-gray-50"
+                        role="menuitem"
+                        tabIndex={-1}
+                        id={`user-menu-item-${index}`}
+                      >
+                        {link.label}
+                      </Link>
+                    ))}
                   </div>
                 )}
               </div>
@@ -164,6 +157,7 @@ const Navbar = () => {
             <div className="px-2 pt-2 pb-3 space-y-1">
               {navigation.map((link, index) => (
                 <Link
+                  key={index}
                   to={link.path}
                   className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                 >
