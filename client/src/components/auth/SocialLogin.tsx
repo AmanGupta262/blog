4409e27b97f9f00bc86f7afc5a2caf1b2b4a6014
Aman Gupta <amang5840@gmail.com>
2021-07-15 18:30:24 +0530

    show facebook login button

--- client/src/components/auth/SocialLogin.tsx
+++ client/src/components/auth/SocialLogin.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useCallback } from "react";
 import {
   GoogleLogin,
   GoogleLoginResponse,
   GoogleLoginResponseOffline,
 } from "react-google-login";
+
+import FacebookLogin from "react-facebook-login-typed";
+
 import { googleLogin } from "../../redux/actions/auth";
 import { useDispatch } from "react-redux";
 
@@ -16,9 +19,10 @@ const SocialLogin = () => {
       dispatch(googleLogin(googleUser.tokenId))
     }
   };
-  const onFailure = (err: any) => {
-    console.log(err);
-  };
+  const responseFacebook = useCallback((response) => {
+    const {accessToken, userID} = response
+    console.log({ accessToken, userID });
+  }, []);
   return (
     <>
       <GoogleLogin
@@ -38,8 +42,25 @@ const SocialLogin = () => {
           </button>
         )}
         onSuccess={onSuccess}
-        onFailure={onFailure}
         cookiePolicy={"single_host_origin"}
+      />
+
+      <FacebookLogin
+        appId={`${process.env.REACT_APP_FACEBOOK_OAUTH_CLIENT_ID}`}
+        
+        callback={responseFacebook}
+        render={(renderProps) => (
+          <button
+            onClick={renderProps.onClick}
+            className="w-11 h-11 items-center justify-center inline-flex rounded-full font-bold text-lg  text-white  bg-blue-900 hover:shadow-lg cursor-pointer transition ease-in duration-300"
+          >
+            <img
+              alt=""
+              className="w-4 h-4"
+              src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHhtbG5zOnN2Z2pzPSJodHRwOi8vc3ZnanMuY29tL3N2Z2pzIiB3aWR0aD0iNTEyIiBoZWlnaHQ9IjUxMiIgeD0iMCIgeT0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBzdHlsZT0iZW5hYmxlLWJhY2tncm91bmQ6bmV3IDAgMCA1MTIgNTEyIiB4bWw6c3BhY2U9InByZXNlcnZlIiBjbGFzcz0iIj48Zz48cGF0aCB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGQ9Im0xNS45OTcgMy45ODVoMi4xOTF2LTMuODE2Yy0uMzc4LS4wNTItMS42NzgtLjE2OS0zLjE5Mi0uMTY5LTMuMTU5IDAtNS4zMjMgMS45ODctNS4zMjMgNS42Mzl2My4zNjFoLTMuNDg2djQuMjY2aDMuNDg2djEwLjczNGg0LjI3NHYtMTAuNzMzaDMuMzQ1bC41MzEtNC4yNjZoLTMuODc3di0yLjkzOWMuMDAxLTEuMjMzLjMzMy0yLjA3NyAyLjA1MS0yLjA3N3oiIGZpbGw9IiNmZmZmZmYiIGRhdGEtb3JpZ2luYWw9IiMwMDAwMDAiIHN0eWxlPSIiIGNsYXNzPSIiPjwvcGF0aD48L2c+PC9zdmc+"
+            />
+          </button>
+        )}
       />
     </>
   );
