b8872e11469611cd0323c97da4d1e936f913f967
Aman Gupta <amang5840@gmail.com>
2021-07-17 23:46:11 +0530

    create update user controller

--- server/controllers/user_controller.ts
+++ server/controllers/user_controller.ts
@@ -1,9 +1,18 @@
-import {Request, Response} from 'express';
+import { Response} from 'express';
+import { IReqAuth } from '../config/interface';
+import User from '../models/user';
 
 const user = {
-    updateUser: async (req: Request, res: Response) => {
+    updateUser: async (req: IReqAuth, res: Response) => {
+        const user = req.user;
+        if(!user) return res.status(401).json({ success: false, msg: "Invalid Authentication." });
+
         try {
-            
+            const { avatar, name } = req.body;
+
+            const updatedUser = await User.findOneAndUpdate({ _id: user._id }, { avatar, name });
+
+            return res.status(200).json({ success: true, msg: "Update success!", user: updatedUser });
         } catch (err: any) {
             return res.json({ success: false, msg: err.message });
         }
