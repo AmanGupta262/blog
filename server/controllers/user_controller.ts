fadffd78d2ed6044fefedca70196eaad05dc648a
Aman Gupta <amang5840@gmail.com>
2021-07-18 12:47:01 +0530

    create controller for password reset

--- server/controllers/user_controller.ts
+++ server/controllers/user_controller.ts
@@ -20,6 +20,23 @@ const user = {
       return res.json({ success: false, msg: err.message });
     }
   },
+  resetPassword: async (req: IReqAuth, res: Response) => {
+    const user = req.user;
+    if (!user)
+      return res
+        .status(401)
+        .json({ success: false, msg: "Invalid Authentication." });
+
+    try {
+      const { password } = req.body;
+
+      await User.findOneAndUpdate({ _id: user._id }, { password });
+
+      return res.status(200).json({ success: true, msg: "Reset password success!" });
+    } catch (err: any) {
+      return res.json({ success: false, msg: err.message });
+    }
+  },
 };
 
 export default user;
--- server/routes/user.ts
+++ server/routes/user.ts
@@ -5,5 +5,6 @@ import userController from "../controllers/user_controller";
 const router = express.Router();
 
 router.patch("/update", auth, userController.updateUser);
+router.patch("/reset_password", auth, userController.resetPassword);
 
 export default router;
